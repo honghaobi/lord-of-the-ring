78d2541705668eff86966c285471ba0261b027ee
henrybi <henrybidesign@gmail.com>
2023-06-19 10:51:04 -0700

    Add ring favicon

--- lord-of-the-ring-henry/src/index.tsx
+++ lord-of-the-ring-henry/src/index.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
 import './index.css';
-import App from './App';
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <h1>Page 1</h1>,
+    },
+    {
+        path: "/page2",
+        element: <h1>Page 2</h1>,
+    }
+]);
+
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    document.getElementById('root') as HTMLElement
 );
 
+root.render(
+    <RouterProvider router={router}/>
+);
