6a9e96a899bf4abd66724ba79a94f8f718a864b0
henrybi <henrybidesign@gmail.com>
2023-06-19 10:39:04 -0700

    Init cra

--- README.md
+++
@@ -1,3 +0,0 @@
-# LibLab Take-Home Project - Frontend
-
-Please modify this readme file to specify how to install, use and test the API.
---
+++ lord-of-the-ring-henry/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Lord of the Ring
+    </div>
+  );
+}
+
+export default App;
